aee59a6fe3707eb62fa93f9dbe595362a0c9d7a5
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-10-21 09:49:13 -0700

    Add files via upload

---
+++ _2_natural.js
@@ -0,0 +1,33 @@
+
+// function sum_no()
+// {
+//     let s=0
+//     for(let i=0;i<=10;i++)
+//     {
+//         s+=i
+
+//     }
+
+    
+//     return s
+    
+// }
+
+// let sum=sum_no()
+// document.writeln(sum)
+
+
+function printno(a,b)
+{
+    let sum=0
+    for(let i=0;i<=10;i++)
+         {
+                 sum+=i
+        
+           }
+
+           return sum;
+}
+
+let add=printno(6,7)
+document.write(add)
