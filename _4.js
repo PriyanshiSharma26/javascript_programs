6c0e3e3279cbe66f910b6d075dd96fc5ebe2bd81
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-10-27 10:28:50 -0700

    Add files via upload

---
+++ _4.js
@@ -0,0 +1,23 @@
+ 
+
+ let p,r,t
+
+let res
+function interst(p,t,r)
+{
+   
+//   p=prompt("enter principle")
+//   r=prompt("enter rate")
+//   t=prompt("enter time")
+
+  res=(p*t*r)/100
+  return res
+
+
+
+}
+p=prompt("enter principle")
+  r=prompt("enter rate")
+  t=prompt("enter time")
+let a=interst(p,t,r)
+document.write(a)
