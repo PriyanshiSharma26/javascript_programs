4b7fc0c5487cea16bcf99f18d515658aef9da3f1
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-10-28 02:43:00 -0700

    Add files via upload

---
+++ arr2.js
@@ -0,0 +1,24 @@
+let arr = [10,20,50,60,30,50];
+
+console.log(arr);
+
+arr.push(1000);
+console.log("After Push : "+arr);
+
+arr.pop();
+console.log("After pop"+arr);
+
+arr.unshift(1000);
+console.log("After Unshift "+arr);
+
+arr.shift();
+console.log("After Shift"+arr);
+
+let arr1 = ["X","Y","Z","A","B","C"];
+arr1.sort();
+console.log(arr1);
+
+
+
+
+
